7aa3a072da277f87971bf4df41c6c47e2f59feef
eesha-barad <eeshabarad49@gmail.com>
2025-03-05 21:45:01 +0530

    Added constructor class

--- Card.java
+++ Card.java
@@ -1,1 +1,12 @@
- 
+ // Card.java
+
+// Represents a single playing card
+public class Card {
+    private String rank;
+    private String suit;
+
+    // Constructor
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
